c4cbc42b69743feaabae16339113e95492cc375b
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-12-11 13:38:42 +0800

    ADD more buttons

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -24,6 +24,8 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
+
+    // Play
     this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
     this.centerButton(this.gameButton, 1);
 
@@ -41,5 +43,37 @@ export default class TitleScene extends Phaser.Scene {
     this.input.on('pointerout', function (event, gameObjects) {
       gameObjects[0].setTexture('blueButton1');
     });
+
+    // Options
+    this.optionsButton = this.add.sprite(300, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.optionsButton);
+
+    this.optionsText = this.add.text(0, 0, 'Options', { fontSize: '32px', fill: '#fff' });
+    this.centerButtonText(this.optionsText, this.optionsButton);
+
+    this.optionsButton.on('pointerdown', function (pointer) {
+      this.scene.start('Options');
+    }.bind(this));
+
+    // Credits
+    this.creditsButton = this.add.sprite(300, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.creditsButton, -1);
+
+    this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
+    this.centerButtonText(this.creditsText, this.creditsButton);
+
+    this.creditsButton.on('pointerdown', function (pointer) {
+      this.scene.start('Credits');
+    }.bind(this));
+
+    this.input.on('pointerover', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton2');
+    });
+
+    this.input.on('pointerout', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton1');
+    });
+
+
   }
 };
