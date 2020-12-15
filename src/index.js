a2b3d79a0f934351ce6d029b8651c91fc134ab34
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-12-15 14:35:36 +0800

    part 1 complete

--- src/index.js
+++ src/index.js
@@ -1,26 +1,180 @@
 import Phaser from 'phaser';
-import config from './config/config';
-import GameScene from './scenes/GameScene';
-import BootScene from './scenes/BootScene';
-import PreloaderScene from './scenes/PreloaderScene';
-import TitleScene from './scenes/TitleScene';
-import OptionsScene from './scenes/OptionsScene';
-import CreditsScene from './scenes/CreditsScene';
-import Model from './Model';
 
-class Game extends Phaser.Game {
-  constructor() {
-    super(config);
-    const model = new Model();
-    this.globals = { model, bgMusic: null };
-    this.scene.add('Boot', BootScene);
-    this.scene.add('Preloader', PreloaderScene);
-    this.scene.add('Title', TitleScene);
-    this.scene.add('Options', OptionsScene);
-    this.scene.add('Credits', CreditsScene);
-    this.scene.add('Game', GameScene);
-    this.scene.start('Boot');
+var BootScene = new Phaser.Class({
+
+  Extends: Phaser.Scene,
+
+  initialize:
+
+    function BootScene() {
+      Phaser.Scene.call(this, { key: 'BootScene' });
+    },
+
+  preload() {
+    // load the resources here
+    // map tiles
+    this.load.image('tiles', 'src/assets/map/spritesheet.png');
+
+    // map in json format
+    this.load.tilemapTiledJSON('map', 'src/assets/map/map.json');
+
+    // our two characters
+    this.load.spritesheet('player', 'src/assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+
+  },
+
+  create() {
+    this.scene.start('WorldScene');
   }
-}
+});
 
-window.game = new Game();
+var WorldScene = new Phaser.Class({
+
+  Extends: Phaser.Scene,
+
+  initialize:
+
+    function WorldScene() {
+      Phaser.Scene.call(this, { key: 'WorldScene' });
+    },
+  preload() {
+
+  },
+  create() {
+    // create your world and bounds here
+    var map = this.make.tilemap({ key: 'map' });
+    var tiles = map.addTilesetImage('spritesheet', 'tiles');
+
+    var grass = map.createStaticLayer('Grass', tiles, 0, 0);
+    var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+    obstacles.setCollisionByExclusion([-1]);
+
+    // set player sprite with bounds
+    this.player = this.physics.add.sprite(50, 100, 'player', 6)
+
+    this.physics.world.bounds.width = map.widthInPixels
+    this.physics.world.bounds.height = map.heightInPixels
+    this.player.setCollideWorldBounds(true)
+
+    // enable keys
+    this.cursors = this.input.keyboard.createCursorKeys()
+
+    // camera setup
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+    this.cameras.main.startFollow(this.player)
+    this.cameras.main.roundPixels = true
+
+    // player animations
+    //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // animation with key 'right'
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14] }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('player', { frames: [0, 6, 0, 12] }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // player can bump into trees 
+    this.physics.add.collider(this.player, obstacles);
+
+    // dummy game objects
+    this.spawns = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
+    for (var i = 0; i < 30; i++) {
+      var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+      var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+      // parameters are x, y, width, height
+      this.spawns.create(x, y, 20, 20);
+    }
+    this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
+
+    onMeetEnemy: (player, zone) => {
+
+      zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width)
+      zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height)
+
+      // shake the world
+      this.cameras.main.shake(300)
+
+      // start battle
+
+
+    }
+  },
+
+  update(time, delta) {
+    this.player.body.setVelocity(0)
+
+
+    // movement in space
+    // horizontal movement
+    if (this.cursors.left.isDown) {
+      this.player.body.setVelocityX(-80)
+    } else if (this.cursors.right.isDown) {
+      this.player.body.setVelocityX(80)
+    }
+    // vertical movement
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-80)
+    } else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(80)
+    }
+
+    // local sprite motion
+    // player movement animations 
+    if (this.cursors.left.isDown) {
+      this.player.anims.play('left', true);
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.anims.play('right', true);
+    }
+    else if (this.cursors.up.isDown) {
+      this.player.anims.play('up', true);
+    }
+    else if (this.cursors.down.isDown) {
+      this.player.anims.play('down', true);
+    }
+    else {
+      this.player.anims.stop();
+    }
+  }
+});
+
+var config = {
+  type: Phaser.AUTO,
+  parent: 'content',
+  width: 320,
+  height: 240,
+  zoom: 2,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: true
+    }
+  },
+  scene: [
+    BootScene,
+    WorldScene
+  ]
+};
+var game = new Phaser.Game(config);
