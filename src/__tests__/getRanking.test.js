1b02cb5b3813887912210c313f93b5a01a626d0f
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-12-22 22:54:23 +0800

    FIX canvas error on testing

---
+++ src/__tests__/getRanking.test.js
@@ -0,0 +1,7 @@
+import getRanking from '../helpers/ranking'
+
+describe('Test getRanking function', () => {
+  it('is expected to be defined', () => {
+    expect(getRanking).toBeDefined()
+  })
+})
