b671e6a2365dd1757edd3db9ede6628d29e1e66a
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-12-22 20:04:49 +0800

    Edit Ranking windows

--- src/scenes/RankingScene.js
+++ src/scenes/RankingScene.js
@@ -25,12 +25,17 @@ export default class OptionsScene extends Phaser.Scene {
 
 
   async create() {
+
+    this.add.text((config.width / 2), 20, `Hero Rankings`, { fontSize: '16px', fill: '#fff' }).setOrigin(0.5)
+
     let res = await getRanking()
     res = res.result
     res = res.sort((a, b) => b.score - a.score).slice(0, 9)
     res.forEach((el, idx) => {
-      this.add.text((config.width / 2), (config.height / 2) + (idx * 15), `${idx + 1}. ${el.user} ${el.score} dragons slayn`, { fontSize: '12px', fill: '#fff' }).setOrigin(0.5)
+      this.add.text((config.width / 2), 40 + (idx * 15), `${idx + 1}. ${el.user} ${el.score} dragons slayn`, { fontSize: '12px', fill: '#fff' }).setOrigin(0.5)
     })
+
+    this.menuButton = new Button(this, config.width / 2, 200, 'blueButton1', 'blueButton2', 'Back', 'Title');
 
   }
 
